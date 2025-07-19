edd738d99709e1ea8d1521a404993f8da7fde91a
SerenaSinha31 <Serena31may@gmail.com>
2025-07-19 15:16:22 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <div>
       <h1>MERN Frontend</h1>
-       -{user?.role === "admin" && <Link to="/admin">Admin</Link>}
+       -----{user?.role === "admin" && <Link to="/admin">Admin</Link>}
       <Link to="/">Home</Link>-<Link to="/cart">MyCart</Link>-
       <Link to="/order">MyOrder</Link>
     
