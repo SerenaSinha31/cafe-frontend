457f867236342eee107b1c9b1ba9610baa387b49
SerenaSinha31 <Serena31may@gmail.com>
2025-07-23 00:29:24 +0530

    update

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import './Admin.css';
 export default function Admin() {
   return (
     <div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -28,34 +28,55 @@
 //   );
 // }
 
-import { useState } from "react";
+// import { useState } from "react";
+// export default function Home() {
+//   const [wicket, setWicket] = useState(0);
+//   const [run, setRun] = useState(0);
+//   const [message, setMessage] = useState();
+//   const incrementRun = () => {
+//     if (wicket < 10) {
+//       setRun(run + 1);
+//       setMessage("Well Done");
+//     }
+//   };
+//   const incrementWicket = () => {
+//     if (wicket < 10) {
+//       setWicket(wicket + 1);
+//       setMessage("Better Luck Next Time");
+//     } else {
+//       setMessage("Game Over");
+//     }
+//   };
+
+//   return (
+//     <>
+//       <button onClick={incrementRun}>Run</button>
+//       <h3>{run}</h3>
+//       <button onClick={incrementWicket}>Wicket</button>
+//       <h3>{wicket}</h3>
+//       <hr />
+//       {message}
+//     </>
+//   );
+// }
+
+
+import "./Home.css";
+
 export default function Home() {
-  const [wicket, setWicket] = useState(0);
-  const [run, setRun] = useState(0);
-  const [message, setMessage] = useState();
-  const incrementRun = () => {
-    if (wicket < 10) {
-      setRun(run + 1);
-      setMessage("Well Done");
-    }
+  const handleClick = () => {
+    alert("Hello");
   };
-  const incrementWicket = () => {
-    if (wicket < 10) {
-      setWicket(wicket + 1);
-      setMessage("Better Luck Next Time");
-    } else {
-      setMessage("Game Over");
-    }
+
+  const handleSubmit = (name) => {
+    alert(`Hello ${name}`);
   };
 
   return (
-    <>
-      <button onClick={incrementRun}>Run</button>
-      <h3>{run}</h3>
-      <button onClick={incrementWicket}>Wicket</button>
-      <h3>{wicket}</h3>
-      <hr />
-      {message}
-    </>
+    <div className="home-container">
+      <h2>Welcome to the Cafe!</h2>
+      <button onClick={handleClick}>Click</button>
+      <button onClick={() => handleSubmit("John")}>Submit</button>
+    </div>
   );
 }
