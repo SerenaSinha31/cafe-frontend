ed312f542fe57963a2429d13906969256f9c442f
SerenaSinha31 <Serena31may@gmail.com>
2025-12-31 18:16:43 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
 
-createRoot(document.getElementById('root')).render(<App/>)
+// createRoot(document.getElementById('root')).render(<App/>)
 
 
 
 
 //what is virtual Dom
 
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App is ready to work offline.");
+  }
+});
+
+
+createRoot(document.getElementById("root")).render(<App />);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "CodeBrew Cafe",
+        short_name: "CodeBrew",
+        description: "MERN Stack",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/icons/CodeBrew_Cafe_192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icons/CodeBrew_Cafe_512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/icons/CodeBrew_Cafe_512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
